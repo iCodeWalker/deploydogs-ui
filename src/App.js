64de1054b990c4713c74e9ad8521c00aff4a42f9
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-04-14 17:10:07 +0530

    Added: git login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
+const CLIENT_ID = "Iv1.9da7059168d6351d";
 
 function App() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get("code");
+
+    console.log(codeParam);
+  });
+  const loginWithGit = () => {
+    window.location.assign(
+      `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`
+    );
+  };
+  const [gitRepo, setRepo] = useState("");
+
+  const [gitRepoName, setGitRepoName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(gitRepo);
+    console.log(gitRepoName);
+
+    axios
+      .get("https://api.github.com/users/iCodeWalker/repos ")
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        console.log("------ process completed --------");
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn" onClick={loginWithGit}>
+        Login with Github
+      </button>
+      <form className="form-container">
+        <input
+          value={gitRepo}
+          onChange={(e) => setRepo(e.target.value)}
+          placeholder="Repo"
+        />
+
+        <input
+          value={gitRepoName}
+          onChange={(e) => setGitRepoName(e.target.value)}
+          placeholder="Repo Name"
+        />
+        <button onClick={handleSubmit} className="btn">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
